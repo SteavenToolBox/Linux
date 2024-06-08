65ce7df0ba452197a78db36af4b06caaca15758e
SteavenGamerYT <omarhanykasban706@gmail.com>
2024-06-08 08:09:21 +0300

    test

--- SteavenToolBoxForLinux/src/main.rs
+++ SteavenToolBoxForLinux/src/main.rs
@@ -9,77 +9,111 @@ fn main() {
         .arg("command -v wget")
         .output()
         .expect("Failed to check if wget is installed.");
-    
+
     if !wget_check.stdout.is_empty() {
         println!("wget is already installed.");
     } else {
         println!("wget could not be found, installing it.");
-        Command::new("sudo")
-            .arg("pacman")
-            .arg("-Syu")
-            .arg("--noconfirm")
-            .arg("--needed")
-            .arg("wget")
-            .status()
-            .expect("Failed to install wget.");
-    }
-    // Check if yay is installed or not
-    let yay_check = Command::new("sh")
-        .arg("-c")
-        .arg("command -v yay")
-        .output()
-        .expect("Failed to check if yay is installed.");
-    
-    if !yay_check.stdout.is_empty() {
-        println!("yay is already installed.");
-    } else {
-        println!("yay could not be found, installing it.");
-        Command::new("sudo")
-            .arg("pacman")
-            .arg("-Syu")
-            .arg("--noconfirm")
-            .arg("--needed")
-            .arg("git")
-            .arg("base-devel")
-            .status()
-            .expect("Failed to install yay.");
-        Command::new("git")
-            .arg("clone")
-            .arg("https://aur.archlinux.org/yay-bin.git")
-            .status()
-            .expect("Failed to clone yay repository.");
-        Command::new("sh")
-            .arg("-c")
-            .arg("cd yay-bin/ && makepkg -si --noconfirm")
-            .status()
-            .expect("Failed to change directory to yay-bin and run makepkg.");
-    }
-    // Check if topgrade is installed or not
-    let topgrade_check = Command::new("sh")
-        .arg("-c")
-        .arg("command -v topgrade")
-        .output()
-        .expect("Failed to check if topgrade is installed.");
-    
-    if !topgrade_check.stdout.is_empty() {
-        println!("topgrade is already installed.");
-    } else {
-        println!("topgrade could not be found, installing it.");
-        Command::new("yay")
-            .arg("-Syu")
-            .arg("--noconfirm")
-            .arg("--needed")
-            .arg("topgrade")
-            .status()
-            .expect("Failed to install topgrade.");
+        let distro = std::fs::read_to_string("/etc/os-release").expect("Failed to read os-release file.");
+        let package_manager = if distro.contains("Arch") {
+            "pacman"
+        } else if distro.contains("Ubuntu") {
+            "apt"
+        } else if distro.contains("Fedora") {
+            "dnf"
+        } else if distro.contains("Debian") {
+            "apt"
+        } else {
+            panic!("Unsupported distribution.");
+        };
+        let mut install_command = match package_manager {
+            "pacman" => {
+                let mut cmd = Command::new("sudo");
+                cmd.arg("pacman").arg("-Syu").arg("--noconfirm").arg("--needed").arg("wget");
+                cmd
+            }
+            "apt" => {
+                let mut cmd = Command::new("sudo");
+                cmd.arg("apt").arg("install").arg("-y").arg("wget");
+                cmd
+            }
+            "dnf" => {
+                let mut cmd = Command::new("sudo");
+                cmd.arg("dnf").arg("install").arg("-y").arg("wget");
+                cmd
+            }
+            _ => panic!("Unsupported package manager."),
+        };
+
+        install_command.status().expect("Failed to install wget.");
     }
 
+    // Check if yay is installed or not
+    let distro = std::fs::read_to_string("/etc/os-release").expect("Failed to read os-release file.");
+    if distro.contains("Arch") {
+        let yay_check = Command::new("sh")
+            .arg("-c")
+            .arg("command -v yay")
+            .output()
+            .expect("Failed to check if yay is installed.");
+
+        if !yay_check.stdout.is_empty() {
+            println!("yay is already installed.");
+        } else {
+            println!("yay could not be found, installing it.");
+            Command::new("sudo")
+                .arg("pacman")
+                .arg("-Syu")
+                .arg("--noconfirm")
+                .arg("--needed")
+                .arg("git")
+                .arg("base-devel")
+                .status()
+                .expect("Failed to install yay.");
+            Command::new("git")
+                .arg("clone")
+                .arg("https://aur.archlinux.org/yay-bin.git")
+                .status()
+                .expect("Failed to clone yay repository.");
+            Command::new("sh")
+                .arg("-c")
+                .arg("cd yay-bin/ && makepkg -si --noconfirm")
+                .status()
+                .expect("Failed to change directory to yay-bin and run makepkg.");
+            }
+        } else {
+            println!("Aur is not supported on this distribution.");
+        }
+    
+    // Check if topgrade is installed or not
+    if distro.contains("Arch") {
+        let topgrade_check = Command::new("sh")
+            .arg("-c")
+            .arg("command -v topgrade")
+            .output()
+            .expect("Failed to check if topgrade is installed.");
+
+        if !topgrade_check.stdout.is_empty() {
+            println!("topgrade is already installed.");
+        } else {
+            println!("topgrade could not be found, installing it.");
+            Command::new("yay")
+                .arg("-Syu")
+                .arg("--noconfirm")
+                .arg("--needed")
+                .arg("topgrade")
+                .status()
+                .expect("Failed to install topgrade.");
+        }
+    } else {
+        println!("Topgrade is not supported on this distribution.");
+    }
 
     while run == 1 {
-        println!("================================================");
-        println!("SteavenToolbox-For-Arch | We care about your pc! Arch Linux!");
-        println!("================================================");
-        println!("1. Update Arch Linux");
+        println!("=============================================================");
+        println!("SteavenToolbox-For-Linux | We care about your pc! Arch Linux!");
+        println!("=============================================================");
+        println!("1. Update Linux");
         println!("2. Install Core Linux Packages required for any desktop environment / window manager");
         println!("3. Install Gnome");
         println!("4. Install Kde");
@@ -97,8 +131,30 @@ fn main() {
         match answer {
             "0" => break,
             "1" => {
-                Command::new("clear").status().expect("Failed to clear screen.");
-                Command::new("topgrade").status().expect("Failed to run topgrade.");
+                let distro = std::fs::read_to_string("/etc/os-release").expect("Failed to read os-release file.");
+                if distro.contains("Arch") {
+                    Command::new("clear").status().expect("Failed to clear screen.");
+                    Command::new("topgrade").status().expect("Failed to run topgrade.");
+                } else if distro.contains("Ubuntu") || distro.contains("Debian") {
+                    Command::new("sudo")
+                        .arg("apt")
+                        .arg("update")
+                        .status()
+                        .expect("Failed to update with apt.");
+                    Command::new("sudo")
+                        .arg("apt")
+                        .arg("upgrade")
+                        .status()
+                        .expect("Failed to upgrade with apt.");
+                } else if distro.contains("Fedora") {
+                    Command::new("sudo")
+                        .arg("dnf")
+                        .arg("update")
+                        .status()
+                        .expect("Failed to update with dnf.");
+                } else {
+                    println!("Distribution not supported for updating.");
+                }
             }
             "2" => {
                 let distro = std::fs::read_to_string("/etc/os-release").expect("Failed to read os-release file.");
@@ -108,13 +164,21 @@ fn main() {
                     "apt"
                 } else if distro.contains("Fedora") {
                     "dnf"
+                } else if distro.contains("Debian") {
+                    "apt"
                 } else {
                     panic!("Unsupported distribution.");
                 };
 
                 let package_file = match package_manager {
                     "yay" => "core-packages-arch.txt",
-                    "apt" => "core-packages-ubuntu.txt",
+                    "apt" => {
+                        if distro.contains("Debian") {
+                            "core-packages-debian.txt"
+                        } else {
+                            "core-packages-ubuntu.txt"
+                        }
+                    },
                     "dnf" => "core-packages-fedora.txt",
                     _ => panic!("Unsupported package manager."),
                 };
@@ -127,8 +191,9 @@ fn main() {
 
                 match package_manager {
                     "yay" => {
-                        Command::new(package_manager)
-                            .args(&["-Syu", "--noconfirm", "--needed", &format!("$(cat {})", package_file)])
+                        Command::new("sh")
+                            .arg("-c")
+                            .arg(format!("{} -Syu --noconfirm --needed < {}", package_manager, package_file))
                             .status()
                             .expect("Failed to install core packages.");
                     }
---
+++ SteavenToolBoxForLinux/target/release/deps/SteavenToolBoxForLinux-8c2e8712a0d6c240.d
@@ -0,0 +1,5 @@
+/home/omarhanykasban/Documents/GitHub/Arch/SteavenToolBoxForLinux/target/release/deps/SteavenToolBoxForLinux-8c2e8712a0d6c240: src/main.rs
+
+/home/omarhanykasban/Documents/GitHub/Arch/SteavenToolBoxForLinux/target/release/deps/SteavenToolBoxForLinux-8c2e8712a0d6c240.d: src/main.rs
+
+src/main.rs:
