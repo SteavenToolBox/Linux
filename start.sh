ddc7ba3e92961bafe4be94077dd5a1809278e4b6
SteavenGamerYT <omarhanykasban706@gmail.com>
2024-06-09 04:39:51 +0300

    agh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget https://github.com/SteavenToolBox/Linux/raw/main/SteavenToolBoxForLinux/target/release/SteavenToolBoxForLinux
+chmod +x ./SteavenToolBoxForLinux
 ./SteavenToolBoxForLinux
