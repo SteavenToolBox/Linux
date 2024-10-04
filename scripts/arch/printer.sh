1144bf94bd86359d5b6dbabb1189393585899ce6
Omar Hany Kasban <omarhanykasban706@gmail.com>
2024-10-04 05:14:11 +0300

    printers.sh arch cups-browsed

--- scripts/arch/printer.sh
+++ scripts/arch/printer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # Cups
-sudo pacman -S --noconfirm --needed cups \
+sudo pacman -S --noconfirm --needed cups cups-browsed \
     foomatic-db-engine foomatic-db foomatic-db-ppds foomatic-db-nonfree foomatic-db-nonfree-ppds \
     gutenprint foomatic-db-gutenprint-ppds \
     hplip \
@@ -9,6 +9,7 @@ sudo pacman -S --noconfirm --needed cups \
 
 # Enable and start Cups
 sudo systemctl enable --now cups
+sudo systemctl enable --now cups-browsed
 
 # Add user to lp group
 sudo usermod -a -G lp $(whoami)
